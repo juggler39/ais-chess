5ab1e96688b2681d39ed15a761df15da4fdccee4
juggler39 <juggler39@yandex.ru>
2020-09-26 12:48:33 +0200

    install chessground and chess.js

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,23 +3,12 @@
     <h1 class="text-center">This is a play human page</h1>
     <v-container>
       <v-row>
-        <v-col class="col-12 col-md-9 grey darken-4">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-            eum error animi, accusamus modi rem aliquid eaque quidem placeat, ut
-            adipisci dicta. Sequi, quos qui deserunt magnam earum dolor eveniet?
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-          </p>
+        <v-col class="col-12 col-md-9 grey darken-4 d-flex justify-center">
+          <Chessboard />
         </v-col>
         <v-col class="col-12 col-md-3 grey darken-4">
           <v-card class="mx-auto">
             <h2 class="text-center">Moves</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores quis nostrum nesciunt quas inventore cumque, minima
-              debitis temporibus eos eligendi incidunt ipsa at corporis
-              voluptatum quod soluta, ab nisi enim?
-            </p>
             <ul>
               <li
                 v-for="(move, index) in $store.state.gameHistory"
@@ -40,10 +29,12 @@
 </template>
 
 <script>
+import Chessboard from "@/components/Chessboard";
 import Chat from "@/components/Chat";
 export default {
   components: {
-    Chat
+    Chat,
+    Chessboard
   }
 };
 </script>
