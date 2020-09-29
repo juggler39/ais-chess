14f7db668f2d197a2d181c260f2aa5f064c96906
Dzianis <imxrayz@gmail.com>
2020-09-29 20:41:11 +0300

    socket io chat realization

---
+++ src/components/chat/ChatItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="chat-item">
+    <div class="user-name">{{ data.name + ":" }}</div>
+    <div class="text">{{ data.text }}</div>
+    <span class="date">{{ data.time }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatItem",
+  props: ["data"]
+};
+</script>
+
+<style scoped>
+.chat-item {
+  display: flex;
+  margin: 3px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(105, 105, 105);
+  padding: 3px;
+  width: 98%;
+  color: azure;
+  font-size: 14px;
+}
+.user-name {
+  color: bisque;
+  font-weight: bold;
+  padding-right: 5px;
+}
+.text {
+  margin: 0;
+  display: inline-block;
+  word-wrap: break-word;
+  width: 60%;
+}
+.date {
+  color: goldenrod;
+  font-size: 12px;
+  text-align: left;
+}
+</style>
--- src/store/modules/gameHistory.js
+++ src/store/modules/gameHistory.js
@@ -1,20 +1,30 @@
 export default {
   state: {
+    chatHistory: [],
     gameHistory: []
   },
   mutations: {
     updateGameHistory(state, turn) {
       state.gameHistory.unshift(turn);
+    },
+    updateChatHistory(state, message) {
+      state.chatHistory.push(message);
     }
   },
   actions: {
     updateHistory(context, turn) {
       context.commit("updateGameHistory", turn);
+    },
+    updateChatHistory(context, message) {
+      context.commit("updateChatHistory", message);
     }
   },
   getters: {
     getHistory(state) {
       return state.gameHistory;
+    },
+    getChatHistory(state) {
+      return state.chatHistory;
     }
   }
 };
