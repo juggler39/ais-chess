17de02193f54323995763f5ee13ba5d9504ca48a
tapeghad <tapeghad@gmail.com>
2020-09-29 20:50:48 +0300

    feat: auth fix (don't load page)

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ router.beforeEach((to, from, next) => {
         return next("/");
       });
     }
-    next();
+    else next();
   }
   verify().catch(() => {console.error; res.json({ err: "error in GAuth" });});
 });
