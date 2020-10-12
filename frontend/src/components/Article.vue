f551c89bb6893bd7973f5fe0430482690e720225
tanyashulha <tsulga6@gmail.com>
2020-10-12 12:01:41 +0300

    feat: article design

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -1,15 +1,15 @@
 <template>
  <article class="col-sm-12">
-  <nav aria-label="breadcrumb">
-    <ul class="breadcrumb">
+  <nav aria-label="breadcrumbs">
+    <ul class="breadcrumbs">
       <li class="breadcrumb-item"><router-link :to="'/'">Home</router-link></li>
+      <span>/</span>
       <li class="breadcrumb-item active" aria-current="page">{{ $route.params.title }}</li>
     </ul>
   </nav>
   <div class="card">
-    <h1>{{ $route.params.title }}</h1>
-    <p>{{ $route.params.content }}</p>
-    <img :src="$route.params.img" alt="">
+    <h1 class="header">{{ $route.params.title }}</h1>
+    <p class="content"><img class="image" :src="$route.params.img" alt="">{{ $route.params.content }}</p>
   </div>
  </article>
 </template>
@@ -22,5 +22,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  
+.breadcrumbs {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+
+  .breadcrumb-item, span {
+    margin: 10px 5px;
+    font-size: 12px;
+    color: #464646;
+    .router-link-active {
+      color: #464646;
+      text-decoration: none;
+      transition: 0.3s;
+      &:hover {
+        color: #7a7a7a;
+      }
+    }
+    &.active {
+      color:  #7a7a7a;
+    }
+  }
+}
+.card {
+  padding: 50px 0 0 0;
+  .content {
+    padding: 50px 0;
+  }
+  .image {
+    float: right;
+    width: 300px;
+    margin: 10px;
+  }
+}
 </style>
