dd7798a6de199484c6aa59b9503c722e06abbaed
juggler39 <juggler39@yandex.ru>
2020-10-23 14:46:27 +0200

    change deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,9 @@
 
 echo "###### Starting Deployment ######"
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+# eval "$(ssh-agent -s)" # Start ssh-agent cache
+# chmod 600 .travis/id_rsa # Allow read access to the private key
+# ssh-add .travis/id_rsa # Add the private key to SSH
 
 echo "###### Continue Deployment ######"
 
