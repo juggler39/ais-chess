10b278f01c95ce01b74975f4f4ca126b0a061767
juggler39 <juggler39@yandex.ru>
2020-10-23 22:35:19 +0200

    change deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,16 +2,18 @@
 
 echo "###### Starting Deployment ######"
 
+git branch
 git config --global push.default simple
 git remote add production ssh://root@$IP:$PORT$DEPLOY_DIR
 git fetch --unshallow || true
-# git push production dev
+git push production master
 
 echo "###### Contunue Deployment ######"
 
 # Skip this command if you don't need to execute any additional commands after deploying.
- ssh root@$IP 
+ ssh root@$IP <<EOF
  cd $DEPLOY_DIR
- git pull origin production
+ mkdir testdir
+EOF
 
 echo "###### End Deployment ######"
