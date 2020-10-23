66b3e52f2c7c9b02e56daeca37be85010b80fe11
juggler39 <juggler39@yandex.ru>
2020-10-23 18:32:07 +0200

    change deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,14 +2,9 @@
 
 echo "###### Starting Deployment ######"
 
-# eval "$(ssh-agent -s)" # Start ssh-agent cache
-# chmod 600 .travis/id_rsa # Allow read access to the private key
-# ssh-add .travis/id_rsa # Add the private key to SSH
-
-echo "###### Continue Deployment ######"
-
 git config --global push.default simple
 git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
+git fetch --unshallow || true
 git push deploy dev
 
 # Skip this command if you don't need to execute any additional commands after deploying.
