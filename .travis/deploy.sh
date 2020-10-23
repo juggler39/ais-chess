f9e3eacb83808daaf848c88f99958b24b6e211fd
juggler39 <juggler39@yandex.ru>
2020-10-23 20:19:37 +0200

    change deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,14 +3,15 @@
 echo "###### Starting Deployment ######"
 
 git config --global push.default simple
-git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
+git remote add production ssh://root@$IP:$PORT$DEPLOY_DIR
 git fetch --unshallow || true
-git push deploy dev
+# git push production dev
+
+echo "###### Contunue Deployment ######"
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-# ssh apps@$IP -p $PORT <<EOF
-#   cd $DEPLOY_DIR
-#   crystal build --release --no-debug index.cr # Change to whatever commands you need!
-# EOF
+ ssh root@$IP 
+ cd $DEPLOY_DIR
+ git pull origin production
 
 echo "###### End Deployment ######"
