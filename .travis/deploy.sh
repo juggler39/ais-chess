24a4ef323480fdfe33f3b740a3c59629976cca99
juggler39 <juggler39@yandex.ru>
2020-10-24 21:53:03 +0200

    change deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,24 +2,6 @@
 
 echo "###### Starting Deployment ######"
 
-# git config --global push.default simple
-# # git remote add production ssh://root@$IP:$PORT$DEPLOY_DIR
-# # git fetch --unshallow || true
-# # git push production dev
-
-# ls
-# cd dist
-# git init
-# git remote add production ssh://root@$IP:$PORT$DEPLOY_DIR/
-# git add .
-# git commit -m 'Build by Travis'
-# git config --list
-# git fetch --unshallow || true
-# git push --force production dev
-
-echo "###### Contunue Deployment ######"
-
-# Skip this command if you don't need to execute any additional commands after deploying.
  ssh root@$IP <<EOF
  cd $DEPLOY_DIR
  cd backend
