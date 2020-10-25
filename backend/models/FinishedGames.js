02ebf2e3cf628348146e9aa9c9b22eb4952f9f28
TapeGhad <61677623+TapeGhad@users.noreply.github.com>
2020-10-25 23:14:14 +0300

    Add and apply eslint (backend) (#169)
    * refactor: add and apply eslint
    
    * refactor: add and apply eslint (backend)
    
    * refactor: eslint

--- backend/models/FinishedGames.js
+++ backend/models/FinishedGames.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const FinishedGameSchema = new Schema({
-  gameDate: { type: Date, default: Date.now },
-  players: { player1ID: String, player1Name: String, player1Color: String, player2ID: String, player2Name: String, player2Color: String },
-  timeToGo: Number,
-  timeWhite: Number,
-  timeBlack: Number,
-  moves: [ Object ],
-  winner: String
+	gameDate: { type: Date, default: Date.now },
+	players: { player1ID: String, player1Name: String, player1Color: String, player2ID: String, player2Name: String, player2Color: String },
+	timeToGo: Number,
+	timeWhite: Number,
+	timeBlack: Number,
+	moves: [ Object ],
+	winner: String
 });
 
 FinishedGameSchema.methods.toJSON = function() {
-    return {
-      id: this._id,
-      gameDate: this.gameDate,
-      players: this.players,
-      timeToGo: this.timeToGo,
-      timeWhite: this.timeWhite,
-      timeBlack: this.timeBlack,
-      moves: this.moves,
-      winner: this.winner
-    };
+	return {
+		id: this._id,
+		gameDate: this.gameDate,
+		players: this.players,
+		timeToGo: this.timeToGo,
+		timeWhite: this.timeWhite,
+		timeBlack: this.timeBlack,
+		moves: this.moves,
+		winner: this.winner
+	};
 };
 
 FinishedGameSchema.index({ players: 1, startDate: -1 });
 
-mongoose.model('FinishedGame', FinishedGameSchema);
+mongoose.model("FinishedGame", FinishedGameSchema);
