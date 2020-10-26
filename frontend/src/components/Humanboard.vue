4ffd451a9a685c2963fc99cb0eb09c1ac6413a88
Dzianis <imxrayz@gmail.com>
2020-10-26 11:24:54 +0300

    conflicts

--- frontend/src/components/Humanboard.vue
+++ frontend/src/components/Humanboard.vue
@@ -39,34 +39,11 @@ export default {
   sockets: {
     newMove(data) {
       //here we are getting every new move
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      // window.localStorage.setItem(
-      //   "playerTurn",
-      //   data.move.color === "w" ? "black" : "white"
-      // );
-      //this.saveTimer();
->>>>>>> ab55158... get moves and timers when not on my game tab #144
-=======
-
->>>>>>> 03627b5... remove commemts
       if (data.move.color === "w") {
         this.timeWhite = data.playerTime;
       } else {
         this.timeBlack = data.playerTime;
       }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      // this.$store.dispatch("updatePvPHistory", data.move);
-      // window.localStorage.setItem(
-      //   "playersHistory",
-      //   JSON.stringify(this.getPVPHistory)
-      // );
->>>>>>> ab55158... get moves and timers when not on my game tab #144
-=======
->>>>>>> 03627b5... remove commemts
       const color = data.move.color === "w" ? "white" : "black";
       //filtering own moves
       if (color !== this.pieceColor) {
