f87ec0a09cc96892739c907d2d1e5b25be4b661e
juggler39 <juggler39@yandex.ru>
2020-10-28 09:14:10 +0200

    change travis configuration

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,8 +9,14 @@ echo "###### Continue Deployment ######"
 ssh root@$IP <<EOF
 
  cd $DEPLOY_DIR/backend
+ echo "MONGO_URL=$MONGO_URL" > .env
+ echo "PORT=8000" >> .env
+ echo "HOST=localhost" >> .env
+ echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID" >> .env
+ echo "SECRET_SESSION=$SECRET_SESSION" >> .env
  npm install
  pm2 restart chess
 EOF
 
 echo "###### End Deployment ######"
+
