10c9fe8381a731670987b66e2402e4e1b582b2cf
tanyashulha <tsulga6@gmail.com>
2020-11-03 23:08:29 +0300

    feat: add component ButtonToTop

---
+++ frontend/src/components/ButtonToTop.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="button-to-top">
+    <v-btn
+      class="elevation-21 btn"
+      small
+      dark
+      fab
+      button
+      right
+      color="blue-grey darken-3"
+      fixed
+      @click="top"
+    >
+      <v-icon>
+        mdi-arrow-up
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonToTop",
+  methods: {
+    top() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.button-to-top {
+  .btn {
+    bottom: 20px;
+    opacity: 0.5;
+  }
+}
+</style>
