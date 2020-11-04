b82800aaf7ecdb6d5f50904a12da5a215bbabb22
tanyashulha <tsulga6@gmail.com>
2020-11-04 22:19:20 +0300

    feat: add fesign for playai

--- frontend/src/views/Playai.vue
+++ frontend/src/views/Playai.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="game">
-    <h1 class="text-center">Play against the Artificial Intelligence</h1>
-    <v-container>
+    <h1 class="text-center mb-5">Play against the Artificial Intelligence</h1>
+    <div class="game-bg">
+      <v-container>
       <v-row>
         <Aiboard
           ref="Aiboard"
@@ -10,18 +11,27 @@
           v-bind:historyMove="historyMove"
           @newGame="newGame.start = $event"
         />
-        <v-col class="col-12 col-md-3 grey darken-4">
-          <GameHistory
+        <v-col class="col-12 col-md-3">
+          <div class="right-column">
+            <GameHistory
             v-bind:moves="getAIHistory"
             @historyMove="historyMove = $event"
           />
-          <v-container>
-            <SetUpAiGame @newGame="newGame = $event" />
-          </v-container>
-          <v-img :src="require('@/assets/giphy.webp')"></v-img>
+          <div class="btn-content">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <div class="content">
+              <p>Do you want to start a new game?</p>
+              <SetUpAiGame @newGame="newGame = $event" />
+            </div>
+          </div>
+          </div>
         </v-col>
       </v-row>
     </v-container>
+    </div>
   </div>
 </template>
 
@@ -59,3 +69,100 @@ export default {
   computed: mapGetters(["getAIHistory"])
 };
 </script>
+
+<style lang="scss" scoped>
+.game-bg {
+  background: #0f0f13e3;
+}
+.right-column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 100%;
+
+  .v-card {
+    width: 100%;
+  }
+}
+.btn-content {
+  position: relative;
+  width: 100%;
+  background: #15151aa6;
+  box-sizing: border-box;
+  overflow: hidden;
+  box-shadow: 0 20px 50px rgb(4, 4, 7);
+  color: white;
+  padding: 20px;
+  border-radius: 4px;
+
+  .content {
+    margin: 15px;
+    background: #15151aa6;
+    padding: 20px;
+    text-align: center;
+    box-shadow: 0 5px 10px rgba(9, 0, 0, 0.5);
+
+    p {
+      @media (max-width: 991.98px) {
+        display: none;
+      }
+    }
+  }
+
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    @media (max-width: 991.98px) {
+      display: none;
+    }
+
+    &:nth-child(1) {
+      transform: rotate(0deg);
+    }
+
+    &:nth-child(2) {
+      transform: rotate(90deg);
+    }
+
+    &:nth-child(3) {
+      transform: rotate(180deg);
+    }
+
+    &:nth-child(4) {
+      transform: rotate(270deg);
+    }
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      background: #3c464b;
+      animation: animate 4s linear infinite;
+    }
+  }
+
+  @keyframes animate {
+    0% {
+      transform: scaleX(0);
+      transform-origin: left;
+    }
+    50% {
+      transform: scaleX(1);
+      transform-origin: left;
+    }
+    50.1% {
+      transform: scaleX(1);
+      transform-origin: right;
+    }
+    100% {
+      transform: scaleX(0);
+      transform-origin: right;
+    }
+  }
+}
+</style>
